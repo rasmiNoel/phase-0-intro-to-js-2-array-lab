537c5fd0d73941a701fb00a5f2fcc2378e013ccd
rasmi <rasminoel@gmail.com>
2023-09-18 09:40:50 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push ("Ralph");
+}
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat() {
+    const cats = ["Broom", ...cats];
+}
+function prependCat() {
+    const cats = [...cats, "Arnold"];
+
+}
+function removeLastCat() {
+    return cats.slice(0, 2);
+}
+function removeFirstCat() {
+    return cats.slice (1);
+}
